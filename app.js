c5d229486f1824e1c69680a4d5da47aa9a2bd7fe
ATechnoHazard <amolele@gmail.com>
2019-08-22 20:50:55 +0530

    Minor refactor
    Signed-off-by: ATechnoHazard <amolele@gmail.com>

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/', routes);
+app.use('/api/v1/', routes);
 app.use(Express.static('public'));
 
 module.exports = app;
